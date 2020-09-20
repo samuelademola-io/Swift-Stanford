1a3981ce510a54fec4ed70238ca169e89b818efb
Samuel F. Ademola <zion@Ebenezer.local>
2020-09-20 04:41:12 -0500

    init commit

---
+++ Memorise/MemoriseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoriseApp.swift
+//  Memorise
+//
+//  Created by Samuel F. Ademola on 9/20/20.
+//
+
+import SwiftUI
+
+@main
+struct MemoriseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
